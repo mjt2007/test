b1f6243c1cf163bb01cd4a5246e7855dde5abbc3
mtang <mtang64@gatech.edu>
2019-02-14 14:05:54 -0500

    change gatech to youtube

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://gatech.edu"
+            href="https://youtube.com"
             target="_blank"
             rel="noopener noreferrer"
           >
